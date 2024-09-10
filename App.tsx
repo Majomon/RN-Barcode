9e3f6740f39f67b4134c262fb795e7eddbd40289
Mauri <mauri.monzon91@gmail.com>
2024-09-09 23:00:18 -0300

    Renderizado de la informacion

--- App.tsx
+++ App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Alert, Text, View} from 'react-native';
+import {Alert, StyleSheet, Text, View} from 'react-native';
 import {RNCamera} from 'react-native-camera';
-
 import QRCodeScanner from 'react-native-qrcode-scanner';
+
 export const App = () => {
+  const [data, setData] = useState('Scan something');
+
   return (
-    <View>
-      <QRCodeScanner
-        onRead={({data}) => console.log(data)}
-        // flashMode={RNCamera.Constants.FlashMode.torch}
-        reactivate={true}
-        reactivateTimeout={500}
-        topContent={
-          <View>
-            <Text>Scanner</Text>
-          </View>
-        }
-      />
-    </View>
+    <QRCodeScanner
+      onRead={({data}) => setData(data)}
+      // flashMode={RNCamera.Constants.FlashMode.torch}
+      reactivate={true}
+      reactivateTimeout={500}
+      topContent={<Text style={styles.centerText}>{data}</Text>}
+    />
   );
 };
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+});
